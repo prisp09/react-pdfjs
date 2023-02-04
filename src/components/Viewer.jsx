6db2044177514cc9191466bf9d993f6023fcca80
Priyanshu Patel <97778509+prisp09@users.noreply.github.com>
2023-02-04 02:13:40 -0500

    Text adding accomplished

--- src/components/Viewer.jsx
+++ src/components/Viewer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import * as pdfjsLib from "pdfjs-dist";
 
-import { PDFDocument, degrees } from "pdf-lib";
+import { PDFDocument, degrees, StandardFonts, rgb } from "pdf-lib";
 
 import "../styles/viewer.css";
 
@@ -10,10 +10,12 @@ function Viewer(props) {
 	const [pdf, setPdf] = React.useState();
 	const [pdfObj, setPdfObj] = React.useState();
 
-	//an array of rotation values for each page that can be use by all useEffects
 	const [rotation] = React.useState([]);
 
+	const[savedText] = React.useState([]);
+
 	React.useEffect(() => {
+		// Load pdfjs object
 		async function loadPDF() {
 			pdfjsLib.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjsLib.version}/pdf.worker.min.js`;
 			await pdfjsLib.getDocument(props.src).promise.then((pdf_) => {
@@ -21,6 +23,7 @@ function Viewer(props) {
 			});
 		}
 
+		// Load pdf-lib object
 		async function loadPDFObj() {
 			const existingPdfBytes = await fetch(props.src).then((res) => res.arrayBuffer());
 			const pdfDoc = await PDFDocument.load(existingPdfBytes);
@@ -35,9 +38,12 @@ function Viewer(props) {
 	React.useEffect(() => {
 		if (pdf && pdfObj) {
 			document.getElementById("Viewer").innerHTML = `
+			<br />
 			<button id="download-button">Download pdf</button>
+			<br />
 			`;
 
+			// Adding PDF pages with buttons to the DOM
 			for (let i = 1; i <= pdf.numPages; i++) {
 				const canvas = document.createElement("canvas");
 				canvas.id = `page_${i}`;
@@ -46,6 +52,10 @@ function Viewer(props) {
 				const rotateButton = document.createElement("button");
 				rotateButton.id = `rotate_${i}`;
 				rotateButton.innerHTML = "Rotate";
+
+				const textButton = document.createElement("button");
+				textButton.id = `text_${i}`;
+				textButton.innerHTML = "Add Text";
 
 				pdf.getPage(i).then((page) => {
 					rotation.push(page._pageInfo.rotate);
@@ -59,6 +69,7 @@ function Viewer(props) {
 					page.render(renderContext);
 					document.getElementById("Viewer").append(document.createElement("br"));
 					document.getElementById("Viewer").append(rotateButton);
+					document.getElementById("Viewer").append(textButton);
 					document.getElementById("Viewer").append(document.createElement("br"));
 					document.getElementById("Viewer").append(canvas);
 					document.getElementById("Viewer").append(document.createElement("br"));
@@ -66,6 +77,8 @@ function Viewer(props) {
 						document.getElementById("Viewer").append(document.createElement("br"));
 					}
 
+					// Adding event listeners to buttons
+					// Rotate button
 					document.getElementById(`rotate_${i}`).addEventListener("click", () => {
 						page._pageInfo.rotate = (page._pageInfo.rotate + 90) % 360;
 						rotation[i - 1] = page._pageInfo.rotate;
@@ -77,6 +90,19 @@ function Viewer(props) {
 							viewport: newViewPort,
 						});
 					});
+
+					// Text button
+					document.getElementById(`text_${i}`).addEventListener("click", () => {
+						const text = prompt("Enter text");
+						if (text) {
+							savedText[i-1] = text;
+							const { width, height } = canvas;
+							const textWidth = ctx.measureText(text).width;
+							const textHeight = 20;
+							ctx.fillStyle = "black";
+							ctx.fillText(text, (width - textWidth) / 2, (height + textHeight) / 2 - 5);
+						}
+					});
 				});
 			}
 			setPdfRendered(true);
@@ -85,16 +111,24 @@ function Viewer(props) {
 
 	React.useEffect(() => {
 		if (pdfRendered) {
-			console.log("pdf rendered");
 			document.getElementById("download-button").addEventListener("click", () => {
-				console.log("download button clicked");
-				console.log(pdfObj);
-				console.log(pdf);
 				const pages = pdfObj.getPages();
-				console.log(pages);
 				for (let i = 0; i < pages.length; i++) {
+					// Rotate pages in the pdf-lib object
 					pages[i].setRotation(degrees(rotation[i]));
+
+					// Add text to pages in the pdf-lib object
+					if(savedText[i]) {
+						pages[i].drawText(savedText[i], {
+							x: pages[i].getWidth() / 2,
+							y: pages[i].getHeight() / 2,
+							size: 8,
+							color: rgb(0, 0, 0)
+						});
+					}
+
 				}
+				// Download the pdf using the pdf-lib object
 				pdfObj.save().then((_pdfBytes) => {
 					const blob = new Blob([_pdfBytes], { type: "application/pdf" });
 					const link = document.createElement("a");
